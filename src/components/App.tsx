32349c6c75186c8edffcc06f3c35faf45de09691
AndreyTara <explorituse@gmail.com>
2024-09-04 21:24:25 +0300

    'adds-files-counter'

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Counter from "./Counter/Counter";
 
 function App() {
   const [count, setCount] = useState<number>(0);
 
   return (
     <>
+      <Counter />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
